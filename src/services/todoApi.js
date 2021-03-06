f062545958eea9d23ccfbb4905cda9289f80ce78
aikins01 <aikinslaryea@gmail.com>
2021-03-06 23:22:19 +0000

    proxy

--- src/services/todoApi.js
+++ src/services/todoApi.js
@@ -1,27 +1,27 @@
 import axios from "axios";
 
 const getAll = () => {
-  return axios.get("https://aikins01.pythonaywhere.com/api/todos");
+  return axios.get("/api/todos");
 };
 
 const get = id => {
-  return axios.get(`https://aikins01.pythonaywhere.com/api/todos/${id}/`);
+  return axios.get(`/api/todos/${id}/`);
 };
 
 const create = data => {
-  return axios.post("https://aikins01.pythonaywhere.com/api/todos/", data);
+  return axios.post("/api/todos/", data);
 };
 
 const update = (id, data) => {
-  return axios.put(`https://aikins01.pythonaywhere.com/api/todos/${id}/`, data);
+  return axios.put(`/api/todos/${id}/`, data);
 };
 
 const down = id => {
-  return axios.delete(`https://aikins01.pythonaywhere.com/api/todos/${id}/`);
+  return axios.delete(`/api/todos/${id}/`);
 };
 
 const downAll = () => {
-  return axios.delete(`https://aikins01.pythonaywhere.com/api/todos/`);
+  return axios.delete(`/api/todos/`);
 };
 
 export default {
