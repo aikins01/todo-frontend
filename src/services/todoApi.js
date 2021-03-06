8443a9f441a181a6669e27918face17921b636f5
aikins01 <aikinslaryea@gmail.com>
2021-03-06 19:51:03 +0000

    http to https for apis

--- src/services/todoApi.js
+++ src/services/todoApi.js
@@ -1,27 +1,27 @@
 import axios from "axios";
 
 const getAll = () => {
-  return axios.get("http://aikins01.pythonaywhere.com/api/todos");
+  return axios.get("https://aikins01.pythonaywhere.com/api/todos");
 };
 
 const get = id => {
-  return axios.get(`http://aikins01.pythonaywhere.com/api/todos/${id}/`);
+  return axios.get(`https://aikins01.pythonaywhere.com/api/todos/${id}/`);
 };
 
 const create = data => {
-  return axios.post("http://aikins01.pythonaywhere.com/api/todos/", data);
+  return axios.post("https://aikins01.pythonaywhere.com/api/todos/", data);
 };
 
 const update = (id, data) => {
-  return axios.put(`http://aikins01.pythonaywhere.com/api/todos/${id}/`, data);
+  return axios.put(`https://aikins01.pythonaywhere.com/api/todos/${id}/`, data);
 };
 
 const down = id => {
-  return axios.delete(`http://aikins01.pythonaywhere.com/api/todos/${id}/`);
+  return axios.delete(`https://aikins01.pythonaywhere.com/api/todos/${id}/`);
 };
 
 const downAll = () => {
-  return axios.delete(`http://aikins01.pythonaywhere.com/api/todos/`);
+  return axios.delete(`https://aikins01.pythonaywhere.com/api/todos/`);
 };
 
 export default {
