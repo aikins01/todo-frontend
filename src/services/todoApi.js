45cb222a0d1fad4b6bc6471dae14dc0afac17fb0
aikins01 <aikinslaryea@gmail.com>
2021-03-06 19:46:56 +0000

    hmmm the links

--- src/services/todoApi.js
+++ src/services/todoApi.js
@@ -1,27 +1,27 @@
 import axios from "axios";
 
 const getAll = () => {
-  return axios.get("/api/todos");
+  return axios.get("http://aikins01.pythonaywhere.com/api/todos");
 };
 
 const get = id => {
-  return axios.get(`/api/todos/${id}/`);
+  return axios.get(`http://aikins01.pythonaywhere.com/api/todos/${id}/`);
 };
 
 const create = data => {
-  return axios.post("/api/todos/", data);
+  return axios.post("http://aikins01.pythonaywhere.com/api/todos/", data);
 };
 
 const update = (id, data) => {
-  return axios.put(`/api/todos/${id}/`, data);
+  return axios.put(`http://aikins01.pythonaywhere.com/api/todos/${id}/`, data);
 };
 
 const down = id => {
-  return axios.delete(`/api/todos/${id}/`);
+  return axios.delete(`http://aikins01.pythonaywhere.com/api/todos/${id}/`);
 };
 
 const downAll = () => {
-  return axios.delete(`/api/todos/`);
+  return axios.delete(`http://aikins01.pythonaywhere.com/api/todos/`);
 };
 
 export default {
