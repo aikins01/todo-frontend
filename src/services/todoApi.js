46be57d4317c9aa3968d52b9bd933c6558dcf566
aikins01 <aikinslaryea@gmail.com>
2021-03-07 02:09:32 +0000

    cors handles

--- src/services/todoApi.js
+++ src/services/todoApi.js
@@ -1,27 +1,27 @@
 import axios from "axios";
-
+import API from '../utils/API'
 const getAll = () => {
-  return axios.get("https://aikins01.herokuapp.com/api/todos");
+  return axios.API.get("/api/todos");
 };
 
 const get = id => {
-  return axios.get(`https://aikins01.herokuapp.com/api/todos/${id}/`);
+  return axios.API.get(`/api/todos/${id}/`);
 };
 
 const create = data => {
-  return axios.post("https://aikins01.herokuapp.com/api/todos/", data);
+  return axios.API.post("/api/todos/", data);
 };
 
 const update = (id, data) => {
-  return axios.put(`https://aikins01.herokuapp.com/api/todos/${id}/`, data);
+  return axios.API.put(`/api/todos/${id}/`, data);
 };
 
 const down = id => {
-  return axios.delete(`https://aikins01.herokuapp.com/api/todos/${id}/`);
+  return axios.API.delete(`/api/todos/${id}/`);
 };
 
 const downAll = () => {
-  return axios.delete(`https://aikins01.herokuapp.com/api/todos/`);
+  return axios.API.delete(`/api/todos/`);
 };
 
 export default {
---
+++ src/utils/API.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "https://aido-back.herokuapp.com/",
+  responseType: "json"
+});
