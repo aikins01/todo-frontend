249a1f781b8d95d6b18814a74520df9cb1ecb0db
aikins01 <aikinslaryea@gmail.com>
2021-03-07 02:49:50 +0000

    syntax error fixed

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,5 +2,5 @@ import axios from "axios";
 export default axios.create({
     baseURL: "https://aido-back.herokuapp.com/",
     responseType: "json",
-    headers: {"Access-Control-Allow-Origin": "*","Access-Control-Allow-Headers: Content-Type, Authorization, Accept, Accept-Language, X-Authorization","Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS","Access-Control-Expose-Headers": "Content-Length, X-JSON",'Access-Control-Max-Age': '86400'}
+    headers: {"Access-Control-Allow-Origin": "*","Access-Control-Allow-Headers": "Content-Type, Authorization, Accept, Accept-Language, X-Authorization","Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS","Access-Control-Expose-Headers": "Content-Length, X-JSON",'Access-Control-Max-Age': '86400'}
   });
