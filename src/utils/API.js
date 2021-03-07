5df9fb4ae3552e5ec7da1babc9cbec8ffa0f4ab6
aikins01 <aikinslaryea@gmail.com>
2021-03-07 03:54:33 +0000

    gg

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 export default axios.create({
     baseURL: "https://aido-back.herokuapp.com/",
-    responseType: "json",
-    headers: {"Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS","Access-Control-Expose-Headers": "Content-Length, X-JSON",'Access-Control-Max-Age': '86400'}
+    responseType: "json"
   });
